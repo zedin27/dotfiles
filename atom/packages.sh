cb9f2d3b537ffaaa4a9ab09b7a89936d213a380a
zedin27 <zeidtisnes@gmail.com>
2018-12-13 17:59:58 -0800

    added my package list

--- atom/packages.sh
+++ atom/packages.sh
@@ -2,12 +2,20 @@
 
 # Hacking Atom WIP
 
-busy-signal@1.4.3
-file-icons@2.1.26
-header-42@0.3.3
-intentions@1.1.5
-language-x86-64-assembly@2.2.11
-linter@2.0.0
-linter-pydocstyle@0.4.7
-linter-ui-default@1.7.1
-platformio-ide-terminal@2.8.4
+# By the way, when you are getting your packages that you want to install and
+# you have the @ and bunch of numbers, attempt using ReGex make your life easier
+
+list= ( "busy-signal" \
+"file-icons" \
+"header-42" \
+"intentions" \
+"language-x86-64-assembly" \
+"linter" \
+"linter-pydocstyle" \
+"linter-ui-default" \
+"platformio-ide-terminal")
+
+for pkgs in ${list[@]}
+do
+	apm install $pkgs
+done
